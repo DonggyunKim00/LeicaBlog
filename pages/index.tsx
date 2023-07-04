628d0dff21ef2a56adcc7f68cc0c89739464a850
ppacman <smwfkim2@naver.com>
2023-07-04 21:32:15 +0900

    [#1] 초기설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-
+import styled from 'styled-components'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -78,7 +78,6 @@ export default function Home() {
               Learn about Next.js in an interactive course with&nbsp;quizzes!
             </p>
           </a>
-
           <a
             href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
@@ -112,3 +111,13 @@ export default function Home() {
     </>
   )
 }
+
+
+
+const Box = styled.div`
+  
+width: 500px;
+height : 450px;
+background-color: black;;
+
+`
