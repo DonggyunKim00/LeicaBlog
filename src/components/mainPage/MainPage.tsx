fce3be5751250f6eb9a343cd1edae1fe603f96dc
ppacman <smwfkim2@naver.com>
2023-07-13 16:17:51 +0900

    [#2] Add mainPage - MainPage 구성 추가

--- src/components/mainPage/MainPage.tsx
+++ src/components/mainPage/MainPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Top from './components/Top';
+import Middle from './components/Middle';
+import Search from './components/Search'
 
 const MainPage = () => {
     return (
         <div>
           <Top />
+          <Search />
+          <Middle />
         </div>
     );
 };
