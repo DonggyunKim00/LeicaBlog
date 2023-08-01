e05b881ef9f8ab09ba3102ad3ec31380938c1043
ppacman <smwfkim2@naver.com>
2023-08-01 09:46:17 +0900

    [#8] feat microScopePage - 헤더 수정 (메인 메뉴 삭제)

--- src/mainPage/MainPage.tsx
+++ src/mainPage/MainPage.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import Top from './components/Top';
-import Search from './components/Search';
-import Footer from './components/Footer';
-import Contents from './components/Contents';
+import React from "react";
+import Top from "./components/Top";
+import Search from "./components/Search";
+import Footer from "./components/Footer";
+import Contents from "./components/Contents";
+import Service from "./components/Service";
+
 const MainPage = () => {
-    return (
-        <div>
-          <Top />
-          <Search />
-          <Contents />
-          <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Service />
+      <Top />
+      <Search />
+      <Contents />
+      <Footer />
+    </div>
+  );
 };
 
 export default MainPage;
-
-
