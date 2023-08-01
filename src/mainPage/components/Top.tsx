14f9002cd8ba5501688cb825015dab84a3b6b01a
ppacman <smwfkim2@naver.com>
2023-08-01 09:47:06 +0900

    [#8] feat microScopePage - 헤더 수정 (메인메뉴 삭제)

--- src/mainPage/components/Top.tsx
+++ src/mainPage/components/Top.tsx
@@ -1,126 +1,59 @@
 import React from "react";
-
 import styled from "styled-components";
 import leicaTypo from "../../../public/img/main/leicaTypo.jpg";
 import Image from "next/image";
-import globe from "../../../public/img/main/globe.png";
-import monitor from "../../../public/img/main/monitor.png";
-import copy from "../../../public/img/main/copy.png";
-import clipboard from "../../../public/img/main/clipboard.png";
-import microscope from "../../../public/img/main/microscope.png";
 import topPicture from "../../../public/img/main/topPicture.png";
 const Top: React.FC = () => {
   return (
     <div>
-      <LeicaTypo>
-        <Image width={160} height={100} alt={""} src={leicaTypo} />
-      </LeicaTypo>
-      <MenuWrapper>
-        <MenuBox>
-          <MenuIcon>
-            <Image width={23} height={23} alt={""} src={globe} />
-          </MenuIcon>
-          <MenuName2>About Leica Microsystems</MenuName2>
-        </MenuBox>
-        <MenuBox>
-          <MenuIcon>
-            <Image width={22} height={22} alt={""} src={clipboard} />
-          </MenuIcon>
-          <MenuName>소식 및 프로모션</MenuName>
-        </MenuBox>
-        <MenuBox>
-          <MenuIcon>
-            <Image width={22} height={20} alt={""} src={monitor} />
-          </MenuIcon>
-          <MenuName>기술자료</MenuName>
-        </MenuBox>
-        <MenuBox>
-          <MenuIcon>
-            <Image width={20} height={20} alt={""} src={copy} />
-          </MenuIcon>
-          <MenuName>견적 및 서비스 문의</MenuName>
-        </MenuBox>
-        <MenuBox>
-          <MenuIcon>
-            <Image width={20} height={20} alt={""} src={microscope} />
-          </MenuIcon>
-          <MenuName>공식 홈페이지</MenuName>
-        </MenuBox>
-      </MenuWrapper>
-      <ContentWrapper>
-        <Image width={917} height={500} alt={""} src={topPicture} />
-      </ContentWrapper>
-      <ScopeMenuWrapper>
-        <ScopeMenuBox>광학 현미경</ScopeMenuBox>
-        <ScopeMenuBox>공초점레이저 현미경</ScopeMenuBox>
-        <ScopeMenuBox>디지털 현미경</ScopeMenuBox>
-        <ScopeMenuBox>현미경 카메라</ScopeMenuBox>
-        <ScopeMenuBox2>수술용 현미경</ScopeMenuBox2>
-      </ScopeMenuWrapper>
-
-      <ScopeMenuWrapper2>
-        <ScopeMenuBox>수퍼해상도 현미경</ScopeMenuBox>
-        <ScopeMenuBox>
-          실체현미경
-          <br />
-          마크로 현미경
-        </ScopeMenuBox>
-        <ScopeMenuBox>현미경 소프트웨어</ScopeMenuBox>
-        <ScopeMenuBox>
-          전자현미경 <br />
-          시료전처리
-        </ScopeMenuBox>
-        <ScopeMenuBox2>교육용 현미경</ScopeMenuBox2>
-      </ScopeMenuWrapper2>
+      <Wrapper>
+        <LeicaTypo>
+          <Image width={160} height={100} alt={""} src={leicaTypo} />
+        </LeicaTypo>
+      
+        <ContentWrapper>
+          <Image width={917} height={500} alt={""} src={topPicture} />
+        </ContentWrapper>
+        <ScopeMenuWrapper>
+          <ScopeMenuBox>광학 현미경</ScopeMenuBox>
+          <ScopeMenuBox>공초점레이저 현미경</ScopeMenuBox>
+          <ScopeMenuBox>디지털 현미경</ScopeMenuBox>
+          <ScopeMenuBox>현미경 카메라</ScopeMenuBox>
+          <ScopeMenuBox2>수술용 현미경</ScopeMenuBox2>
+        </ScopeMenuWrapper>
+        <ScopeMenuWrapper2>
+          <ScopeMenuBox>수퍼해상도 현미경</ScopeMenuBox>
+          <ScopeMenuBox>
+            실체현미경
+            <br />
+            마크로 현미경
+          </ScopeMenuBox>
+          <ScopeMenuBox>현미경 소프트웨어</ScopeMenuBox>
+          <ScopeMenuBox>
+            전자현미경 <br />
+            시료전처리
+          </ScopeMenuBox>
+          <ScopeMenuBox2>교육용 현미경</ScopeMenuBox2>
+        </ScopeMenuWrapper2>
+      </Wrapper>
     </div>
   );
 };
 
 export default Top;
 
+const Wrapper = styled.div`
+  display: flexbox;
+`;
 const MenuBox = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 170px;
-  height: 80px;
-  cursor: pointer;
-  transition: background-color 0.3s;
-
-  &:hover {
-    background-color: #eeeeee;
-  }
-
-  &::before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 5px;
-    background-color: rgb(127, 145, 157);
-  }
+  position: sticky;
+  top: 0;
+  right: 0;
+  width: 200px;
+  height: 100px;
+  border: 1px solid black;
 `;
 
-const MenuIcon = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 15px;
-  margin-bottom: 5px;
-`;
-
-const MenuName = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 15x;
-  font-weight: 550;
-  white-space: nowrap;
-  color: #686868;
-`;
 const MenuName2 = styled.div`
   display: flex;
   justify-content: center;
@@ -135,24 +68,6 @@ const LeicaTypo = styled.div`
   display: flex;
   justify-content: center;
   margin-bottom: 10px;
-`;
-
-const MenuWrapper = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: space-between;
-  width: 914px;
-  margin: 0 auto;
-
-  &::before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 1px;
-    background-color: rgb(127, 145, 157);
-  }
 `;
 
 const ContentWrapper = styled.div`
@@ -241,10 +156,4 @@ const ScopeMenuBox2 = styled.div`
   &:hover {
     background-color: #b3babd;
   }
-`;
-
-const MenuOption = styled.div`
-  width: 500px;
-  height: 100px;
-  background-color: gray;
 `;
