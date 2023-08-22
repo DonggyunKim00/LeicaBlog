d0d0bea2c671be8897afde4200464f1f82044d1f
ppacman <smwfkim2@naver.com>
2023-08-22 19:36:05 +0900

    [#2] feat mainPage - 메인 페이지 구성 수정

--- src/mainPage/MainPage.tsx
+++ src/mainPage/MainPage.tsx
@@ -4,12 +4,14 @@ import Search from "./components/Search";
 import Footer from "./components/Footer";
 import Contents from "./components/Contents";
 import Service from "./components/Service";
+import Inquiry from "./components/Inquiry";
 
 const MainPage = () => {
   return (
     <div>
-      <Service />
+     
       <Top />
+      {/* <Inquiry /> */}
       <Search />
       <Contents />
       <Footer />
