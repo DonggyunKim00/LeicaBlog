14a6f2bbb86bf3d31d26d6354dc1195b7a5ed92d
donggyun <donggyunkim12@gmail.com>
2023-09-06 13:23:34 +0900

    fix: Header,Search,Footer 공통컴포넌트에 따라 Layout 적용

--- src/container/mainPage/MainPage.tsx
+++ src/container/mainPage/MainPage.tsx
@@ -1,16 +1,10 @@
 import React from "react";
-import Top from "../../components/Top";
-import Search from "../../components/Search";
-import Footer from "../../components/Footer";
 import Contents from "./components/Contents";
 
 const MainPage = () => {
   return (
     <div>
-      <Top />
-      <Search />
       <Contents />
-      <Footer />
     </div>
   );
 };
--- src/container/microScopePage/microScopePage.tsx
+++ src/container/microScopePage/microScopePage.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import Top from "../../components/Top";
-import Footer from "../../components/Footer";
-import Search from "../../components/Search";
 import ContentsList from "./components/ContentsList";
 import MicroContents from "./components/MicroContents";
 import CreateContent from "./components/CreateContent";
@@ -9,12 +6,9 @@ import CreateContent from "./components/CreateContent";
 const MicroScopePage = () => {
   return (
     <div>
-      <Top />
-      <Search />
       <CreateContent />
       <ContentsList />
       <MicroContents />
-      <Footer />
     </div>
   );
 };
