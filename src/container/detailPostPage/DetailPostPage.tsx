b377e282fc5da69eb0d2af330b5c20c89806d11a
donggyun <donggyunkim12@gmail.com>
2023-09-06 13:23:34 +0900

    fix: Header,Search,Footer 공통컴포넌트에 따라 Layout 적용

--- src/container/detailPostPage/DetailPostPage.tsx
+++ src/container/detailPostPage/DetailPostPage.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-import Top from "../../components/Top";
-import Search from "../../components/Search";
-import Footer from "../../components/Footer";
 import ContentsList from "@/container/microScopePage/components/ContentsList";
 import Content from "./components/Content";
 const DetailPostPage = () => {
   return (
     <>
-      <Top />
-      <Search />
       <ContentsList />
       <Content />
-      <Footer />
     </>
   );
 };
