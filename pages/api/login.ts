40bd31e100b775cc2d71be11d1856cd2c1438cda
donggyun <donggyunkim12@gmail.com>
2023-09-15 16:48:13 +0900

    feat: LoginPage ui 개발, login api 연결 #26

---
+++ pages/api/login.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { InputForm } from "@/container/adminLoginPage/LoginPage";
+export const adminLoginApi = async (form: InputForm) => {
+  try {
+    const formData = new FormData();
+    formData.append("username", form.id);
+    formData.append("password", form.password);
+
+    const res = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/login`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+
+    return res;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
