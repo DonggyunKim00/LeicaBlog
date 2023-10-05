90c6429c94c280ee9cb48d1821de3558c0a3e486
donggyun <donggyunkim12@gmail.com>
2023-10-05 16:05:46 +0900

    feat: 페이지네이션 훅 추가 #19

--- src/container/searchPage/components/Content.tsx
+++ src/container/searchPage/components/Content.tsx
@@ -1,11 +1,10 @@
-import { useSearchBoard } from "@/hooks/boardHook/useBoard";
+import { useSearchBoard } from "@/hooks/pagenateHook/usePagenate";
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
-import { styled } from "styled-components";
+import React from "react";
+import { css, styled } from "styled-components";
 import Board, { BoardProps } from "./Board";
 import { BiSolidLeftArrow, BiSolidRightArrow } from "react-icons/bi";
-// <BiSolidLeftArrow size="4" />;
-// <BiSolidRightArrow size="4" />;
+import { useSearchBoardData } from "@/hooks/boardHook/useBoard";
 
 interface PageButtonProps {
   $isactive: boolean;
@@ -13,9 +12,26 @@ interface PageButtonProps {
 
 const Content = () => {
   const router = useRouter();
+  const keyword = router.query.keyword?.toString() || "";
+  const page = Number(router.query.page) || 1;
 
-  const { keyword } = router.query;
-  const { findBoard } = useSearchBoard(keyword?.toString() || "");
+  const { findBoard } = useSearchBoardData({
+    keyword: keyword,
+    size: 1,
+    page: page,
+  });
+
+  const {
+    currentPage,
+    handlePageChange,
+    pages,
+    handleNextGroup,
+    handlePrevGroup,
+    lastPageGroup,
+    pageGroups,
+  } = useSearchBoard({
+    apiData: findBoard,
+  });
 
   return (
     <Container>
@@ -30,40 +46,69 @@ const Content = () => {
         {findBoard && findBoard.size == 0 ? (
           <NoDataBoard>{` "${router.query.keyword}" 검색결과가 없습니다.`}</NoDataBoard>
         ) : (
-          // pageData &&
-          // pageData.map((item: BoardProps, idx: number) => {
-          //   return (
-          //     <Board
-          //       id={item.id}
-          //       thumbnail={item.thumbnail}
-          //       title={item.title}
-          //       childName={item.childName}
-          //       content={item.content}
-          //       createTime={item.createTime}
-          //       subTitle={item.subTitle}
-          //       key={idx}
-          //     />
-          //   );
-          // })
-          <></>
+          <>
+            {findBoard.childList.map((item: BoardProps, idx: number) => {
+              return (
+                <Board
+                  id={item.id}
+                  thumbnail={item.thumbnail}
+                  title={item.title}
+                  parentName={item.parentName}
+                  childName={item.childName}
+                  content={item.content}
+                  createTime={item.createTime}
+                  subTitle={item.subTitle}
+                  key={idx}
+                />
+              );
+            })}
+          </>
         )}
       </BoardList>
-      <PageBoxContainer>
-        <PageBox>
-          <Page>
-            {/* {Array.from({ length: totalPages }, (_, index) => (
-              <PageButton
-                key={index + 1}
-                $isactive={currentPage === index + 1}
-                onClick={() => handlePageChange(index + 1)}
-                value={currentPage}
-              >
-                {index + 1}
-              </PageButton>
-            ))} */}
-          </Page>
-        </PageBox>
-      </PageBoxContainer>
+      {findBoard.childList[0] && (
+        <PageBoxContainer>
+          <PageBox>
+            <Page>
+              {pageGroups !== 0 && (
+                <PrevBtn
+                  onClick={() => {
+                    handlePrevGroup(pageGroups);
+                  }}
+                >
+                  <BiSolidLeftArrow size="5" />
+                  <span>이전</span>
+                </PrevBtn>
+              )}
+              {pages ? (
+                pages.map((item: number) => {
+                  return (
+                    <PageButton
+                      key={item}
+                      $isactive={currentPage === item}
+                      onClick={() => handlePageChange(item)}
+                      value={currentPage}
+                    >
+                      {item}
+                    </PageButton>
+                  );
+                })
+              ) : (
+                <></>
+              )}
+              {pageGroups !== lastPageGroup && (
+                <NextBtn
+                  onClick={() => {
+                    handleNextGroup(pageGroups);
+                  }}
+                >
+                  <span>다음</span>
+                  <BiSolidRightArrow size="5" />
+                </NextBtn>
+              )}
+            </Page>
+          </PageBox>
+        </PageBoxContainer>
+      )}
     </Container>
   );
 };
@@ -133,4 +178,19 @@ const PageButton = styled.button<PageButtonProps>`
   &:hover {
     background-color: #eeeeee; /* 호버 시 보더 스타일 정의 */
   }
+`;
+const pagingBtn = css`
+  width: 44px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2px;
+  padding: 0px 8px;
+`;
+
+const NextBtn = styled.button`
+  ${pagingBtn}
+`;
+const PrevBtn = styled.button`
+  ${pagingBtn}
 `;
