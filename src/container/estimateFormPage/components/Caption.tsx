9a80c7dc48dceee4eddb1fedc3409a660d87d9b7
donggyun <donggyunkim12@gmail.com>
2023-10-16 14:12:26 +0900

    style: estimatePage 스타일변경

--- src/container/estimateFormPage/components/Caption.tsx
+++ src/container/estimateFormPage/components/Caption.tsx
@@ -9,7 +9,7 @@ const Caption = () => {
       <ImageBox>
         <Image src={caption} width={650} height={280.95} alt={""} />
       </ImageBox>
-      <H1>라이카 견적 및 서비스 문의</H1>
+      <H1>견적 및 서비스 문의</H1>
       <Span>
         우측 양식을 작성해 주시면, 조속히 요청하신 사항에 대해 연락을 드리도록
         하겠습니다. Comment 란에 제품명, 모델명 및 자세한 요청사항 등을 기입해
@@ -19,12 +19,21 @@ const Caption = () => {
       <Span>
         Leica Microsystems는 일반 현미경, 공초점 레이저 현미경, 이미징 시스템,
         샘플 전처리 장비, 의료 장비 사업 분야에서 시장을 선도합니다. 시스템이
-        모듈식이므로 사용자 고유의 니즈에 맞추어 시스템을 구성할 수 있습니다.{" "}
+        모듈식이므로 사용자 고유의 니즈에 맞추어 시스템을 구성할 수 있습니다.
       </Span>
       <Button>LEICA 제품 자세히 보기</Button>
-      <H2>(주) 라이카코리아</H2>
-      <Span>서울시 강남구 영동대로 741 (청담동) 은성빌딩 6층</Span>
-      <Span>Tel 02-3416-4430 | Fax 02-514-6548</Span>
+      <H2>엔티에스</H2>
+      <BottomSpan>
+        본사 : 대전광역시 유성구 테크노2로 314, 202-1호 (탑립동)
+      </BottomSpan>
+      <BottomSpan>
+        서울사무소 : 서울특별시 금천구 남부순환로112길 27(가산동)
+      </BottomSpan>
+      <BottomSpan>
+        <b>문의 사항</b>
+        <br />
+        Tel 010-6778-5445 | 본사 042-931-1012 | FAX 042-931-1013
+      </BottomSpan>
     </Wrapper>
   );
 };
@@ -51,12 +60,17 @@ const H1 = styled.div`
 const Span = styled.div`
   width: 650px;
   line-height: 1.5;
-  margin-bottom: 40px;
+  margin-bottom: 30px;
+`;
+const BottomSpan = styled.div`
+  width: 650px;
+  line-height: 1.5;
+  margin-bottom: 10px;
 `;
 const H2 = styled.div`
   font-size: 24px;
   font-weight: 600;
-  margin-bottom: 40px;
+  margin-bottom: 20px;
 `;
 
 const Button = styled.button`
@@ -67,5 +81,5 @@ const Button = styled.button`
   padding: 10px 20px;
   font-size: 16px;
   cursor: pointer;
-  margin-bottom : 40px;
+  margin-bottom: 40px;
 `;
--- src/container/estimateFormPage/components/EstimateForm.tsx
+++ src/container/estimateFormPage/components/EstimateForm.tsx
@@ -97,153 +97,154 @@ const EstimateForm = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-    <EstimateBox>
-      <ContactWrapper>
-        <ContactBox>CONTACT US</ContactBox>
-      </ContactWrapper>
-      <Line>
-        <Wrapper>
-          <Label htmlFor="name">이름 *</Label>
-          <Input
-            id="name"
-            name="name"
-            value={userData.name}
-            onChange={handleInputChange}
-            required
-          />
-        </Wrapper>
-        <Wrapper>
-          <Label htmlFor="surname">성 *</Label>
-          <Input
-            id="surname"
-            name="surname"
-            value={userData.surname}
-            onChange={handleInputChange}
-            required={true}
-          />
-        </Wrapper>
-      </Line>
-      <Line>
-        <Wrapper>
-          <Label htmlFor="email">이메일 *</Label>
-          <Input
-            id="email"
-            name="email"
-            value={userData.email}
-            onChange={handleInputChange}
-            required
-          />
-        </Wrapper>
-        <Wrapper>
-          <Label htmlFor="phone">휴대폰번호 *</Label>
-          <Input
-            id="phone"
-            name="phone"
-            value={userData.phone}
-            onChange={handleInputChange}
-            required
-          />
-        </Wrapper>
-      </Line>
-      <Line>
-        <Wrapper>
-          <Label htmlFor="company">회사/기관 *</Label>
-          <Input
-            id="company"
-            name="company"
-            value={userData.company}
-            onChange={handleInputChange}
-            required
-          />
-        </Wrapper>
-        <Wrapper>
-          <Label htmlFor="position">직책 *</Label>
-          <Input
-            id="position"
-            name="position"
-            value={userData.position}
-            onChange={handleInputChange}
-            required
-          />
-        </Wrapper>
-      </Line>
-      <Line>
-        <Wrapper>
-          <Label htmlFor="region">지역 *</Label>
-          <Input
-            id="region"
-            name="region"
-            value={userData.region}
-            onChange={handleInputChange}
-            required
-          />
-        </Wrapper>
-        <Wrapper>
-          <Label htmlFor="country">국가 *</Label>
-          <Input
-            id="country"
-            name="country"
-            value={userData.country}
-            onChange={handleInputChange}
-            required
-          />
-        </Wrapper>
-      </Line>
+      <EstimateBox>
+        <ContactWrapper>
+          <ContactBox>CONTACT US</ContactBox>
+        </ContactWrapper>
+        <Line>
+          <Wrapper>
+            <Label htmlFor="name">이름 *</Label>
+            <Input
+              id="name"
+              name="name"
+              value={userData.name}
+              onChange={handleInputChange}
+              required
+            />
+          </Wrapper>
+          <Wrapper>
+            <Label htmlFor="surname">성 *</Label>
+            <Input
+              id="surname"
+              name="surname"
+              value={userData.surname}
+              onChange={handleInputChange}
+              required={true}
+            />
+          </Wrapper>
+        </Line>
+        <Line>
+          <Wrapper>
+            <Label htmlFor="email">이메일 *</Label>
+            <Input
+              id="email"
+              name="email"
+              value={userData.email}
+              onChange={handleInputChange}
+              required
+            />
+          </Wrapper>
+          <Wrapper>
+            <Label htmlFor="phone">휴대폰번호 *</Label>
+            <Input
+              id="phone"
+              name="phone"
+              value={userData.phone}
+              onChange={handleInputChange}
+              required
+            />
+          </Wrapper>
+        </Line>
+        <Line>
+          <Wrapper>
+            <Label htmlFor="company">회사/기관 *</Label>
+            <Input
+              id="company"
+              name="company"
+              value={userData.company}
+              onChange={handleInputChange}
+              required
+            />
+          </Wrapper>
+          <Wrapper>
+            <Label htmlFor="position">직책 *</Label>
+            <Input
+              id="position"
+              name="position"
+              value={userData.position}
+              onChange={handleInputChange}
+              required
+            />
+          </Wrapper>
+        </Line>
+        <Line>
+          <Wrapper>
+            <Label htmlFor="region">지역 *</Label>
+            <Input
+              id="region"
+              name="region"
+              value={userData.region}
+              onChange={handleInputChange}
+              required
+            />
+          </Wrapper>
+          <Wrapper>
+            <Label htmlFor="country">국가 *</Label>
+            <Input
+              id="country"
+              name="country"
+              value={userData.country}
+              onChange={handleInputChange}
+              required
+            />
+          </Wrapper>
+        </Line>
 
-      <Label htmlFor="request">
-        기타 요청사항이나 문의 사항이 있으면 기재해주세요.
-      </Label>
-      <RequestBox>
-        <RequestInput
-          id="request"
-          name="request"
-          value={userData.request}
-          onChange={handleInputChange}
-        />
-      </RequestBox>
-      <PrivacyLabel>
-        상기의 개인정보(이메일,연락처)를 라이카마이크로시스템즈에서 아래와 같은
-        목적으로 수집/활용하는 것에 동의합니다. <br />
-        - 관련 제품, 서비스, 워크샵 등에 대한 정보를 전화/이메일/문자메시지를
-        통해 제공
-        <br />- 서비스 개선/개발을 위한 만족도조사 및 통계처리 *
-      </PrivacyLabel>
-      <RadioButtonGroup>
-        <RadioBox>
-          <RadioButton
-            type="radio"
-            id="agreeYes"
-            name="agreePrivacyPolicy"
-            value="yes"
-            checked={userData.agreePrivacyPolicy === "yes"}
+        <Label htmlFor="request">
+          기타 요청사항이나 문의 사항이 있으면 기재해주세요.
+        </Label>
+        <RequestBox>
+          <RequestInput
+            id="request"
+            name="request"
+            value={userData.request}
             onChange={handleInputChange}
-            required
           />
-          <RadioLabel htmlFor="agreeYes">예</RadioLabel>
-        </RadioBox>
+        </RequestBox>
+        <PrivacyLabel>
+          상기의 개인정보(이메일,연락처)를 엔티에스에서 아래와 같은 목적으로
+          수집/활용하는 것에
+          <br /> 동의합니다. <br />
+          - 관련 제품, 서비스, 워크샵 등에 대한 정보를 전화/이메일/문자메시지를
+          통해 제공
+          <br />- 서비스 개선/개발을 위한 만족도조사 및 통계처리 *
+        </PrivacyLabel>
+        <RadioButtonGroup>
+          <RadioBox>
+            <RadioButton
+              type="radio"
+              id="agreeYes"
+              name="agreePrivacyPolicy"
+              value="yes"
+              checked={userData.agreePrivacyPolicy === "yes"}
+              onChange={handleInputChange}
+              required
+            />
+            <RadioLabel htmlFor="agreeYes">예</RadioLabel>
+          </RadioBox>
 
-        <RadioBox>
-          <RadioButton
-            type="radio"
-            id="agreeNo"
-            name="agreePrivacyPolicy"
-            value="no"
-            checked={userData.agreePrivacyPolicy === "no"}
-            onChange={handleInputChange}
-          />
-          <RadioLabel htmlFor="agreeNo">아니오</RadioLabel>
-        </RadioBox>
-      </RadioButtonGroup>
-      <AgreeDiv>
-        아래의 (지금 전송) 버튼 클릭함으로써 라이카마이크로시스템즈의 <br />
-        <RedText>이용약관과 개인정보 취급방침</RedText>을 검토하고 동의하였음을
-        확인합니다. <br />
-        또한 본인의 개인정보에 대한 선택을 라이카마이크로시스템즈의
-        <br />
-        <RedText>개인정보 취급방침</RedText>에 따라 처리하는것에 동의합니다.
-      </AgreeDiv>
-      <SubmitButton type="submit">지금 전송</SubmitButton>
-    </EstimateBox>
+          <RadioBox>
+            <RadioButton
+              type="radio"
+              id="agreeNo"
+              name="agreePrivacyPolicy"
+              value="no"
+              checked={userData.agreePrivacyPolicy === "no"}
+              onChange={handleInputChange}
+            />
+            <RadioLabel htmlFor="agreeNo">아니오</RadioLabel>
+          </RadioBox>
+        </RadioButtonGroup>
+        <AgreeDiv>
+          아래의 (지금 전송) 버튼 클릭함으로써 엔티에스의 <br />
+          <RedText>이용약관과 개인정보 취급방침</RedText>을 검토하고
+          동의하였음을 확인합니다. <br />
+          또한 본인의 개인정보에 대한 선택을 엔티에스의
+          <br />
+          <RedText>개인정보 취급방침</RedText>에 따라 처리하는것에 동의합니다.
+        </AgreeDiv>
+        <SubmitButton type="submit">지금 전송</SubmitButton>
+      </EstimateBox>
     </form>
   );
 };
