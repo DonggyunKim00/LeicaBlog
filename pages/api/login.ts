a5ece95fedc38fcdcaf510cdb6b4b76f1894109b
donggyun <donggyunkim12@gmail.com>
2023-11-21 22:59:10 +0900

    fix(login.ts): 서버측 로그인 api 수정에 따른 로직 수정 #56

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -4,20 +4,11 @@ import secureLocalStorage from "react-secure-storage";
 
 export const adminLoginApi = async (form: InputForm) => {
   try {
-    const formData = new FormData();
-    formData.append("username", form.id);
-    formData.append("password", form.password);
-
-    const res = await axios.post(
-      `${process.env.NEXT_PUBLIC_API_URL}/login`,
-      formData,
-      {
-        withCredentials: true,
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      }
-    );
+    const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
+      memberId: form.id,
+      password: form.password,
+      withCredentials: true,
+    });
 
     // 로그인 성공할시 kimyounggil이라는 string을 Buffer로 암호화하여 세션스토리지에 저장
     if (res.status == 200) {
