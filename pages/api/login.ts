df3e309660b4941656deaacb2e34ffb35cfb1a4d
donggyun <donggyunkim12@gmail.com>
2023-11-23 20:38:57 +0900

    chore(login.ts): 테스트를 위한 로그인 폼데이터 형식 재적용 #56

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,16 +1,23 @@
 import axios from "axios";
 import { InputForm } from "@/container/adminLoginPage/LoginPage";
 import { encrypt } from "@/utils/crypto";
-// import secureLocalStorage from "react-secure-storage";
 
 export const adminLoginApi = async (form: InputForm) => {
   try {
-    const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
-      memberId: form.id,
-      password: form.password,
-      withCredentials: true,
-    });
+    const formData = new FormData();
+    formData.append("username", form.id);
+    formData.append("password", form.password);
 
+    const res = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/login`,
+      formData,
+      {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
     // 로그인 성공할시 Nts-Leica-Login 이라는 string을 AES 암호화 알고리즘으로 암호화하여 세션스토리지에 저장
     if (res.status == 200) {
       alert("관리자 로그인 성공");
