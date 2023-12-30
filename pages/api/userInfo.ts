6a17a8ac59a6fc9b58aad884c4a22d165dd6849d
donggyun <donggyunkim12@gmail.com>
2023-12-30 23:00:30 +0900

    feat(AdminProvider,getUserInfo): 유저 정보를 받는 api에 따라 isAdmin 로직 재 구현 #56

--- pages/api/userInfo.ts
+++ pages/api/userInfo.ts
@@ -1,6 +1,19 @@
 import axios from "axios";
 import axiosInstance from "./axiosInstance";
 
+export const getUserInfo = async () => {
+  try {
+    const res: any = await axiosInstance.get(`/info`, {
+      headers: {
+        Authorization: sessionStorage.getItem("access"),
+      },
+    });
+    return res;
+  } catch (err) {
+    return err;
+  }
+};
+
 export const checkJwt = async () => {
   try {
     const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/expired`, {
--- src/components/AdminProvider.tsx
+++ src/components/AdminProvider.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, useEffect, useState } from "react";
+import { getUserInfo } from "../../pages/api/userInfo";
 
 export const AdminContext = createContext({
   isAdmin: false,
@@ -8,11 +9,16 @@ export const AdminProvider: React.FC<React.PropsWithChildren> = (props) => {
   const [isAdmin, setIsAdmin] = useState<boolean>(false);
 
   useEffect(() => {
-    const refreshToken = sessionStorage.getItem("access");
-    if (refreshToken) {
-      setIsAdmin(true);
-    }
+    const userRole = async () => {
+      const res = await getUserInfo();
+      if (res.data === "ADMIN") {
+        setIsAdmin(true);
+      }
+    };
+    userRole();
   }, []);
+
+  console.log(isAdmin);
 
   return (
     <AdminContext.Provider value={{ isAdmin }}>
